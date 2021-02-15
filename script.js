e30dda26fb65c06dae84339aea71ee9458f03cd3
Balthazard Matéo <mateo.balthazard@hotmail.com>
2021-02-16 00:59:05 +0100

    added sound effects

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ const board = document.getElementById('board') // ON RECUPERE LE PLATEAU DE JEU
 const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+var success = document.getElementById("success");
 let circleTurn // VARIABLE DE CHANGEMENT DE JOUEUR
+//const compteur = document.getElementById('compteur').innerHTML = count + "Coups joués"
 
 startGame()
 
@@ -52,10 +54,12 @@ function handleClick(e) {
 
 function endGame(draw) { //AFFICHE LE MESSAGE DE FIN SELON LE RESULTAT
     if (draw) {
-      winningMessageTextElement.innerText = 'Egalite !'
+      winningMessageTextElement.innerText = 'Egalite !';
+      success.play();
     } else {
         winningMessageTextElement.innerText = `${circleTurn ? "O" :
         "X"} Est le vainqueur!`
+        success.play();
     }
     winningMessageElement.classList.add('show')
 }
@@ -99,6 +103,114 @@ function playCount() { // COMPTEUR DE COUPS JOUES
     if (typeof 'cell x' || typeof 'cell circle') {
     
            count = countX.length + countCircle.length
-           console.log(count)
        }
 }
+
+class State {
+    constructor(startTimestamp, difference, suspended) {
+      this.startTimestamp = startTimestamp;
+      this.difference = difference;
+      this.suspended = suspended;
+    }
+  
+    static ready() {
+      return new State(null, 0, 0);
+    }
+  }
+
+  function play() {
+    var boop = document.getElementById("boop");
+    boop.play();
+    
+
+}
+
+
+  
+  // ICI C'EST LE TIMER ----------------------------------------------------TIMER------------------------------------------------------
+  // class Stopwatch {
+  //   constructor(state) {
+  //     this.state = state;
+  //     this.requestAnimationId = null;
+  //     this.handleClickStart = this.handleClickStart.bind(this);
+  //     document
+  //       .getElementById("board")             //---------------------------------------------------------start---------------
+  //       .addEventListener("click", this.handleClickStart);
+  //     this.handleClickStop = this.handleClickStop.bind(this);
+  //     document
+  //       .getElementById("pause")
+  //       .addEventListener("click", this.handleClickStop);
+  
+  //     this.handleClickReset = this.handleClickReset.bind(this);
+  //     document
+  //       .getElementById("restartButton")
+  //       .addEventListener("click", this.handleClickReset);
+  //     this.tick = this.tick.bind(this);
+  //     this.render();
+  //   }
+  
+  //   static ready() {
+  //     return new Stopwatch(State.ready());
+  //   }
+  
+  //   setState(newState) {
+  //     this.state = { ...this.state, ...newState };
+  //     this.render();
+  //   }
+  
+  //   tick() {
+  //     this.setState({
+  //       difference: new Date(new Date() - this.state.startTimestamp)
+  //     });
+  //     this.requestAnimationId = requestAnimationFrame(this.tick);
+  //   }
+  
+  //   handleClickStart() {
+  //     if (this.state.startTimestamp) {
+  //       // Prevent multi clicks on start
+  //       return;
+  //     }
+  //     this.setState({
+  //       startTimestamp: new Date() - this.state.suspended,
+  //       suspended: 0
+  //     });
+  //     this.requestAnimationId = requestAnimationFrame(this.tick);
+  //   }
+  
+  //   handleClickStop() {
+  //     cancelAnimationFrame(this.requestAnimationId);
+  //     this.setState({
+  //       startTimestamp: null,
+  //       suspended: this.state.difference
+  //     });
+  //   }
+  
+  //   handleClickReset() {
+  //     cancelAnimationFrame(this.requestAnimationId);
+  //     this.setState(State.ready());
+  //   }
+  
+  //   render() {
+  //     const { difference } = this.state;
+  //     const hundredths = (difference
+  //       ? Math.floor(difference.getMilliseconds() / 10)
+  //       : 0
+  //     )
+  //       .toString()
+  //       .padStart(2, "0");
+  //     const seconds = (difference ? Math.floor(difference.getSeconds()) : 0)
+  //       .toString()
+  //       .padStart(2, "0");
+  //     const minutes = (difference ? Math.floor(difference.getMinutes()) : 0)
+  //       .toString()
+  //       .padStart(2, "0");
+  
+  //     // Render screen
+  //     document.getElementById("minutes").textContent = minutes;
+  //     document.getElementById("seconds").textContent = seconds;
+  //     document.getElementById("hundredths").textContent = hundredths;
+  //   }
+  // }
+  
+  // const STOPWATCH = Stopwatch.ready();
+
